d4130234a1d9c799fc2fdb59ff4a0026f9b4fe15
rcrespo9 <rudycrespo@hushmail.com>
2013-10-08 14:55:14 -0400

    some changes

--- scripts/todo.js
+++ scripts/todo.js
@@ -1,103 +1,78 @@
 var todo_app = {
 
-	createTask: function() {
+  createTask: function() {
+    todo_item.setTaskText();
+  },
 
-		// var date = new Date();
-		// var month = date.getMonth();
-		// var day = date.getDay();
-		// var year = date.getFullYear();
-		// var hours = date.getHours();
-		// var minutes = date.getMinutes();
-		// var seconds = date.getSeconds();
+  timeAlerts: function(task_name, task_due_date_time) {
 
-		// var formattedDate = (parseInt(month) + 1) + '/' + day + '/' + year;
-		// var formattedTime = hours + ':' + minutes + ':' + seconds;
-		// console.log(formattedDate);
-		// console.log(formattedTime);
+    var completed_button = document.getElementById('completed_button' + task_name)
 
-		todo_item.setTaskText();
+    if (completed_button) {
+      var due_date = new Date(task_due_date_time);
+      var now = new Date();
+      var time_left = due_date - now
 
-		}
+      if (time_left > 60000) {
+        document.getElementById(task_name).style.color = "green";
+      } else if (time_left > 30000){
+        document.getElementById(task_name).style.color = "yellow";
+      } else if (time_left <= 30000 && time_left > 0) {
+        document.getElementById(task_name).style.color = "orange";
+      } else {
+		clearTimeout();
+        document.getElementById(task_name).style.color = "red";
+		alert("You missed your deadline!")
+      }
+
+      console.log(time_left);
+
+      setTimeout(function() {
+        todo_app.timeAlerts(task_name,task_due_date_time);
+      }, 1000)
+    }
+  },
 };
 
 var todo_item = {
+  
+  setTaskText: function() {
+    var task_name = document.getElementById('new-task-field').value;
+    var task_due_date_time = new Date(document.getElementById('due-date-time-field').value);
+    
+    todo_item.appendTask(task_name, task_due_date_time);
+    
 
-	setTaskText: function() {
-		var task_name = document.getElementById('new-task-field').value;
-		var task_date_time = document.getElementById('date-time-field').value;
-		var time_now = Date.now();
+  },
 
-		task_date_time = new Date(task_date_time);
+  appendTask: function(task_name, task_due_date_time) {
+    if (task_name) {
+      var todo_items = document.getElementById('todo-items');
+      var entry = document.createElement('li');
+      entry.setAttribute("id", task_name)
 
-		todo_item.appendTask(task_name, task_date_time);
+      completed_button = todo_item.completedButton(task_name);
 
-		time_left = task_date_time - time_now
+      delete_button = todo_item.deleteButton(task_name);
+      
 
-	if(time_left * .25) {	
-	setInterval(function() {
-		document.getElementById("todo-items").style.background = "green";
-	}, 1000);
-	};	
+      entry.appendChild(document.createTextNode(task_name));
+      entry.appendChild(document.createTextNode(" Due date: " + task_due_date_time));
+      entry.appendChild(completed_button);
+      entry.appendChild(delete_button);
+    
+      todo_items.appendChild(entry);
 
-	if(time_left * .50) {
-	setInterval(function() {
-		document.getElementById("todo-items").style.background = "yellow";
-	}, 1000);
-	};
+      document.getElementById("completed_button" + task_name).addEventListener('click', function(event) {
+        todo_item.completedTask(entry, task_name);
+      });
 
-	if(time_left * .75) {
-	setInterval(function() {
-		document.getElementById("todo-items").style.background = "orange";
-	}, 1000);
-	};
+      document.getElementById("delete_button" + task_name).addEventListener('click', function(event) {
+        todo_item.deleteTask(entry, task_name);
+      });
 
-	if(time_left < 0) {
-	setInterval(function() {
-		document.getElementById("todo-items").style.background = "red";
-		alert("This task is due!")
-	}, 1000);
-	};
+      todo_app.timeAlerts(task_name, task_due_date_time);
 
-	// // setInterval(function() {
-	// 	document.getElementById("todo-items").style.background = "orange";
-	// // }, time_left * .75);
-
-	// // setInterval(function() {
-	// 	document.getElementById("todo-items").style.background = "red";
-	// // }, time_left * 1.0);
-			
-
-	},
-
-	appendTask: function(task_name, task_date_time) {
-		if (task_name) {
-			var todo_items = document.getElementById('todo-items');
-			var entry = document.createElement('li');
-			entry.setAttribute("id", "entry" + task_name)
-
-
-			completed_button = todo_item.completedButton(task_name);
-
-			delete_button = todo_item.deleteButton(task_name);
-
-			// formatted_time = formattedTime;
-			// formatted_date = formattedDate;
-
-			entry.appendChild(document.createTextNode(task_name));
-			entry.appendChild(document.createTextNode(" | Due Date: " + task_date_time));
-			entry.appendChild(completed_button);
-			entry.appendChild(delete_button);
-			// entry.appendChild(formatted_time);
-			// entry.appendChild(formatted_date);
-			todo_items.appendChild(entry);
-
-			document.getElementById("completed_button" + task_name).addEventListener('click', function(event) {
-				todo_item.completedTask(entry, task_name);
-			});
-
-			document.getElementById("delete_button" + task_name).addEventListener('click', function(event) {
-				todo_item.deleteTask(entry, task_name);
-			});
 
 		} else {
 			alert('nope!');
